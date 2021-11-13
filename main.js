37649d273aaf5ac7764ed6c19860b7453678953a
yttap8 <moc.liamg.patrick@gmail.com>
2021-11-12 23:43:19 -0500

    final changes

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/*
+1. Describe the purpose, function, and input and output of your program.
+The purpose of my program is to check whether a password is strong depending on if it has a number, letter, and special characters.
+In addition, it must not be common and at least 8 characters long. The function is to categorize the password into different
+strength categories. The input is the password, and the ouput is the strength in addition to the problems with it.
+Furthermore, my program is also able to generate a random password.
+
+2. How did you use abstraction in you code?
+I used abstraction by assigining different strenghts to numerical values. Furthermore, the password is turned into a string that
+functions as an array so that each character can be checked.
+
+3. How did you manage complexity by using arrays & loops? (I.e. how did you avoid using making your code repetitive and complicated by using arrays and loops?)
+I managed complexity by essentially treating the password like an array. This way, I could use a for loop to check through each element of the array,
+in other words, each character of the password, to see if they fit the password requirements. Furthermoe, I also used the special charAt function to make it
+easier to check for characters.
+*/
+
+
 //Sets global variables for strength, the message that will be displayed, and the feedback that will be displayed about strength
 var strength = 0;
 var message = "";
@@ -124,4 +142,26 @@ function checkPassword(){
   }
   document.getElementById("Message").innerHTML = message;
   document.getElementById("Feedback").innerHTML = feedback;
+}
+
+
+//extra credit: character generator
+//pool of all characters that can be chosen for random password
+var pool= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789(!@#$%^&*('
+
+//generates a random combination of characters from pool depending on input length
+function generatePassword(length){
+  let password = "";
+  for (var i = 0; i<length; i++){
+    var random = Math.round(Math.random()* pool.length);
+    var character = pool.charAt(random);
+    password += character;
+  }
+  return password;
+}
+
+//runs the generatePassword function when button is clicked, makes the password 12 characters and displays it
+function displayRndPass(){
+  password = generatePassword(12);
+  document.getElementById("RndPass").innerHTML = "Your Random Password Is: " + password;
 }
